15cd1104a003c8d73fc265eedd3823a22f1d13b4
mrrrlisn <mrrrrlisin@gmail.com>
2018-06-20 14:21:11 +0300

    aa

---
+++ Assets/Scripts/Flower.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flower : MonoBehaviour
+{
+
+    Animator anim;
+    bool isCLose = false;
+
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+	}
+
+    private void OnMouseDown()
+    {
+        isCLose = !isCLose;
+        anim.SetBool("IsClose", isCLose);
+    }
+
+}
